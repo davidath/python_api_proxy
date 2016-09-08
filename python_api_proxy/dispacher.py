b941807ce85525070c8a23b7e1d75391f1d900f9
david <david@david.lan>
2016-09-08 13:49:59 +0300

    Added exceptions when event id is not valid

--- python_api_proxy/dispacher.py
+++ python_api_proxy/dispacher.py
@@ -25,18 +25,26 @@ class C42Dispacher(object):
            "Content-type": "application/json",
            "Authorization": "Token " + self.api_token,
           }
-          # GET event_details request and contents
-          details_request = urllib2.Request(evt_details_url,headers=req_headers)
-          details_contents = urllib2.urlopen(details_request).read()
-          # GET event_with_sub request and contents
-          sub_request = urllib2.Request(evt_sub_url,headers=req_headers)
-          sub_contents = urllib2.urlopen(sub_request).read()
-          # return responses from C42 API as json
-          return json.loads(details_contents),json.loads(sub_contents)
+          try:
+              # GET event_details request and contents
+              details_request = urllib2.Request(evt_details_url,headers=req_headers)
+              details_contents = urllib2.urlopen(details_request).read()
+              # GET event_with_sub request and contents
+              sub_request = urllib2.Request(evt_sub_url,headers=req_headers)
+              sub_contents = urllib2.urlopen(sub_request).read()
+              # return responses from C42 API as json
+              return json.loads(details_contents),json.loads(sub_contents)
+          except: #If id is not valid
+              return 'Event id not valid'
           
       def combine_and_cache(self,event_id):
-          #Get C42 API responses
-          details,subscriptions = self.c42_api_call(event_id)
+          #Get C42 API responses if event id is valid
+          try:
+             details,subscriptions = self.c42_api_call(event_id)
+          except: #if not return error
+             response = {}
+             response['Error'] = 'Event id not valid'
+             return response
           details_data = details['data']
           # although the data array only contains one element(in this case) i dont want to use "magic number" and access it by details_data[0]
           for event_data in details_data:
@@ -65,7 +73,10 @@ class C42Dispacher(object):
              return self.combine_and_cache(event_id)
           #If class is not running for the first time then compare timestamps
           #Extract cached response id
-          cache_json = json.loads(self.cached_response)
+          try: #Check if cached response has a valid event id
+             cache_json = json.loads(self.cached_response)
+          except:
+             return self.combine_and_cache(event_id)
           #If timestamp hasn't passed 4.2 minutes and user requests the same event then return cached response
           if (time.time() - self.start_t < self.cache_time_interval) and (event_id == cache_json['id']):
                 #although timestamp comparing is not needed i'll leave it there for proof that the response is cached for 4.2 minutes
